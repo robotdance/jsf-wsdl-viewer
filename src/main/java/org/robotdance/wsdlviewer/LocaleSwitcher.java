ce18c4ee0fee00ed7d26f9181e66b518ee375efb
MarcosMenegazzo <correiodomarcos@gmail.com>
2016-11-14 14:29:11 -0200

    Fixed I18n issues regarding Country and Language

--- src/main/java/org/robotdance/wsdlviewer/LocaleSwitcher.java
+++ src/main/java/org/robotdance/wsdlviewer/LocaleSwitcher.java
@@ -1,5 +1,6 @@
 package org.robotdance.wsdlviewer;
 
+import java.io.Serializable;
 import java.util.Locale;
 
 import javax.annotation.PostConstruct;
@@ -9,24 +10,37 @@ import javax.faces.context.FacesContext;
 
 @ManagedBean
 @SessionScoped
-public class LocaleSwitcher {
-    private Locale locale;
+public class LocaleSwitcher implements Serializable {
+
+  private static final long serialVersionUID = 385950061416176524L;
+  
+  private Locale locale;
     
-    @PostConstruct
-    public void init() {
-        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
-    }    
-    
-    public String change(String lang, String country) {
-        setLocale(new Locale(lang, country));
-        return FacesContext.getCurrentInstance().getViewRoot().getViewId() + "?faces-redirect=true";
-    }
+  @PostConstruct
+  public void init() {
+      locale = stripLocale(FacesContext.getCurrentInstance().getExternalContext().getRequestLocale());
+  }    
+  
+  public String change(String lang) {
+      setLocale(new Locale(lang));
+      return FacesContext.getCurrentInstance().getViewRoot().getViewId() + "?faces-redirect=true";
+  }
 
 	public Locale getLocale() {
 		return locale;
 	}
 
+  public Locale getSimplifiedLocale() {
+    locale = stripLocale(locale);
+    return locale;
+  }
+	
 	public void setLocale(Locale locale) {
 		this.locale = locale;
 	}
+	
+	private Locale stripLocale(Locale locale) {
+	  return new Locale(locale.getLanguage());
+	}
+	
 }
